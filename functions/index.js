c91139258c18677e0aa4fcfd41d2f8a95210b886
pure-js <3690368+pure-js@users.noreply.github.com>
2018-05-26 23:25:18 +0300

    add cors support

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
 const functions = require('firebase-functions');
+// CORS Express middleware to enable CORS Requests.
+const cors = require('cors')({
+  origin: true,
+});
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.isCorrectAnswer = functions.https.onRequest((request, response) => {
- response.send("false");
+  return cors(request, response, () => {
+    response.send("false");
+  });
 });
