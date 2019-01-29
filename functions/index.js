7b47b1ff85f147b545d58d4e0f541e8c06b968d1
pure-js <3690368+pure-js@users.noreply.github.com>
2019-01-29 20:24:30 +0300

    create randomQuestion function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,22 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.questions = functions.database.collection(collection).get();
+const floatToInteger = (max, float) => Math.floor(float * (max + 1));
+
+exports.randomQuestion = functions.https.onCall(
+  (collection, context) => {
+    return admin.firestore().collection(collection).get()
+      .then(querySnapshot => {
+        const rand = floatToInteger(querySnapshot.size, Math.random());
+        const docIds = querySnapshot.docs.map(doc => doc.id);
+        return querySnapshot.docs[rand - 1].data();
+      });
+  }
+);
 
 exports.isCorrectAnswer = functions.https.onCall(
   (data, context) => {
