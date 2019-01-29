256add48e1bb4287d5f0c956ec739a4794c8f73b
pure-js <3690368+pure-js@users.noreply.github.com>
2019-01-30 01:23:22 +0300

    change region to west europe

--- functions/index.js
+++ functions/index.js
@@ -7,22 +7,26 @@ admin.initializeApp();
 
 const floatToInteger = (max, float) => Math.floor(float * (max + 1));
 
-exports.randomQuestion = functions.https.onCall(
-  (collection, context) => {
-    return admin.firestore().collection(collection).get()
-      .then((querySnapshot) => {
-        const rand = floatToInteger(querySnapshot.size, Math.random());
-        const doc = querySnapshot.docs[rand - 1];
-        return Object.assign({id: doc.id}, doc.data());
-      });
-  }
-);
-
-exports.isCorrectAnswer = functions.https.onCall(
-  (data, context) => {
-    const random_boolean = Math.random() >= 0.5;
-    return {
-      'correct': random_boolean
+exports.randomQuestion = functions
+  .region('europe-west1')
+  .https.onCall(
+    (collection, context) => {
+      return admin.firestore().collection(collection).get()
+        .then((querySnapshot) => {
+          const rand = floatToInteger(querySnapshot.size, Math.random());
+          const doc = querySnapshot.docs[rand - 1];
+          return Object.assign({id: doc.id}, doc.data());
+        });
     }
-  }
-);
+  );
+
+exports.isCorrectAnswer = functions
+  .region('europe-west1')
+  .https.onCall(
+    (data, context) => {
+      const random_boolean = Math.random() >= 0.5;
+      return {
+        'correct': random_boolean
+      }
+    }
+  );
